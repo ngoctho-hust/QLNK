d20a861ff46ef7279474bdda6037e1ddb95e8cb0
Ngoc Tho <ngoctho.t1k54@gmail.com>
2019-11-25 15:32:12 +0700

    add eclipse

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,9 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.NhanKhau;
+import model.SoHoKhau;
+
+import java.util.ArrayList;
 
 public class Main extends Application {
 
+    public ArrayList<SoHoKhau> soHoKhauArrayList;
+    public ArrayList<NhanKhau> nhanKhauArrayList;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/quanLyHoKhau.fxml"));
